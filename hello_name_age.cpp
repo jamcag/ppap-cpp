59dcebe22aa3ca234b7e0537b3cac6db8a090259
JC <jamcag@proton.me>
2023-02-18 21:27:01 -0500

    Modify hello_name_age to print age in months

--- hello_name_age.cpp
+++ hello_name_age.cpp
@@ -3,9 +3,10 @@
 int main()
 {
     cout << "Please enter your first name and age\n";
-    string first_name; // string variable
-    int age; // integer variable
+    string first_name = "???"; // string variable
+    double age = 0; // numeric variable
     cin >> first_name; // read a string
     cin >> age; // read an integer
-    cout << "Hello, " << first_name << " (age " << age << ")\n";
+    double age_in_months = age * 12;
+    cout << "Hello, " << first_name << " (age " << age_in_months << " months)\n";
 }
